94fdc49ecd726bee2d3fa3f26fad0827aaa49d2d
Whaleep <billy_1817@yahoo.com.tw>
2021-08-01 02:56:58 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db=require('./models')
+const db = require('./models')
 const app = express()
 const port = 3000
 
@@ -8,6 +8,8 @@ const port = 3000
 // express-handlebars 在 2019/5/15 v3.1.0 之後，{defaultLayout: 'main'}已經是預設值
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   // 前台入口
@@ -19,4 +20,6 @@ module.exports = app => {
   // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
   app.get('/admin/restaurants', adminController.getRestaurants)
 
+  app.get('/signup',userController.signUpPage)
+  app.post('/signup',userController.signUp)
 }
