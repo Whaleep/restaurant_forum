0bb46a38523150eda50e2ffeea9ee0a8eb6a775a
Whaleep <billy_1817@yahoo.com.tw>
2021-08-01 22:56:50 +0800

    Install multer and add image upload feature

---
+++ migrations/20210801141314-add-image-to-restaurant.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Add altering commands here.
+    await queryInterface.addColumn('Restaurants','image',{
+      type: Sequelize.STRING
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Add reverting commands here.
+    await queryInterface.removeColumn('Restaurants','image')
+  }
+};
