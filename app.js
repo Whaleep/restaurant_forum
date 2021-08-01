0be68af8e20961b5ee00603b0571953f797be6bf
Whaleep <billy_1817@yahoo.com.tw>
2021-08-01 09:11:32 +0800

    Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -8,8 +10,18 @@ const port = 3000
 // express-handlebars 在 2019/5/15 v3.1.0 之後，{defaultLayout: 'main'}已經是預設值
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+// setup bodyParser
 app.use(express.urlencoded({ extended: true }))
+// setup session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// 把 req.flash 放到 res.locals 裡面
+app.use((req,res,next)=>{
+  res.locals.success_messages=req.flash('success_massages')
+  res.locals.error_messages=req.flash('error_messages')
+  next()
+})
+
 
 app.listen(port, () => {
   db.sequelize.sync()
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,19 +3,35 @@ const db = require('../models')
 const User = db.User
 
 const userController = {
-// 註冊頁面
+  // 註冊頁面
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-// 註冊操作
+  // 註冊操作
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+    // confirm password 兩次輸入的密碼要相同
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      // confirm unique user 信箱不能重複
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_messages', '信箱重複！')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name: req.body.name,
+              email: req.body.email,
+              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+            }).then(user => {
+              req.flash('success_messages','成功註冊帳號！')
+              return res.redirect('/signin')
+            })
+          }
+        })
+    }
   }
 }
 
