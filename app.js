00f27dbc86c2585c9433147698e3555acdceb483
Whaleep <billy_1817@yahoo.com.tw>
2021-08-23 17:41:23 +0800

    Add imgur

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const session = require('express-session')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
+
 
 // 設定 view engine 使用 handlebars
 // express-handlebars 在 2019/5/15 v3.1.0 之後，{defaultLayout: 'main'}已經是預設值
